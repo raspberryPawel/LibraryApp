7b348681da9a2301fe1092477baf77f5ad4d30a3
Paweł Malina <p.malina@getprintbox.com>
2022-02-04 20:05:45 +0100

    added validation to books form

--- Models/Book.cs
+++ Models/Book.cs
@@ -1,28 +1,30 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace LibApp.Models
 {
     public class Book
     {
         public int Id { get; set; }
-		[Required]
-		[StringLength(255)]
+		[Required(ErrorMessage = "Please enter a book name")]
+		[StringLength(255, ErrorMessage = "Name must be shorten then 255 characters")]
+		[Display(Name = "Book name:")]
 		public string Name { get; set; }
 
-		[Required]
+		[Required(ErrorMessage = "Please enter a book author")]
+		[Display(Name = "Autor name and surname (or alias):")]
 		public string AuthorName { get; set; }
-		[Required]
+		[Required(ErrorMessage = "Please select genre")]
 		public Genre Genre { get; set; }
-		[Required]
-		[Display(Name = "Genre")]
+		[Required(ErrorMessage = "Please select genre")]
+		[Display(Name = "Book genre:")]
 		public byte GenreId { get; set; }
 		public DateTime DateAdded { get; set; }
-		[Display(Name="Realease Date")]
+		[Display(Name="Realease Date:")]
+		[Required(ErrorMessage = "Please enter a book release date")]
 		public DateTime ReleaseDate { get; set; }
+		[Required(ErrorMessage = "Please enter number in stock value")]
+		[Display(Name = "Number of books in stock:")]
 		public int NumberInStock { get; set; }
 		public int NumberAvailable { get; set; }
 	}
